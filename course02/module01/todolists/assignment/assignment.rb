385370d6d88916a3866b40b1fef3778455a2b7d5
Alejandro Guevara <alejandro.guevara.esc@gmail.com>
2019-02-04 02:35:53 -0600

    Add creation functionality for User and TodoList

--- course02/module01/todolists/assignment/assignment.rb
+++ course02/module01/todolists/assignment/assignment.rb
@@ -2,20 +2,14 @@ require_relative '../config/environment'
 
 # Use Active Record Model methods to implement the following methods.
 class Assignment
-
-  #
-  # Insert rows in DB
-  #
   def create_user(params)
-      # accept a hash of user properties (`:username` and `:password_digest`) as an input parameter. Note these are 100% same as model class.
-      # use the User Model class to create a new user in the DB
-      # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
+    user = User.new(username: params[:username], password_digest: params[:password_digest])
+    user.save
   end
 
   def create_todolist(params)
-      # accept a hash of todolist properties (`:name` and `:due_date`) as an input parameter. Note these are not 100% the same as Model class.
-      # use the TodoList Model class to create a new user in the DB
-      # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
+    todo_list = TodoList.new(list_name: params[:name], list_due_date: params[:due_date])
+    todo_list.save
   end
 
   #
