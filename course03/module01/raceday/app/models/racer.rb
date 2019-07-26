c4f020a720cc40f3037e2dc0ce6a869a309e83a9
Alejandro Guevara <alejandro.guevara.esc@gmail.com>
2019-07-25 22:42:57 -0500

    Model finished

--- course03/module01/raceday/app/models/racer.rb
+++ course03/module01/raceday/app/models/racer.rb
@@ -1,4 +1,6 @@
 class Racer
+  include ActiveModel::Model
+
   attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
 
   def initialize(params={})
@@ -12,7 +14,7 @@ class Racer
   end
 
   def self.find(id)
-    result = self.collection.find({ _id: id }).first
+    result = self.collection.find({ _id: BSON::ObjectId.from_string(id.to_s) }).first
     result.nil? ? nil : Racer.new(result)
   end
 
@@ -31,4 +33,41 @@ class Racer
       .skip(skip)
       .limit(limit.to_i)
   end
+
+  def save
+    result = self.class.collection.insert_one({
+      number: @number, first_name: @first_name, last_name: @last_name, gender: @gender,
+      group: @group, secs: @secs
+    })
+    @id = result.inserted_id.to_s
+  end
+
+  def update(params)
+    @number = params[:number].to_i
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @group = params[:group]
+    @gender = params[:gender]
+    @secs = params[:secs].to_i
+
+    params.slice!(:number, :first_name, :last_name, :gender, :group, :secs)
+    self.class.collection.find(_id: BSON::ObjectId.from_string(@id)).replace_one(params)
+  end
+
+  def destroy
+    self.class.collection.find(_id: BSON::ObjectId.from_string(@id)).delete_one
+  end
+
+  def persisted?
+    @id.present?
+  end
+
+  def created_at
+    nil
+  end
+
+  def updated_at
+    nil
+  end
+
 end
